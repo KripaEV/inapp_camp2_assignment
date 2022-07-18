372e4f96a021a6e34a6e75a326bc048d3c1d7451
KripaVS <kripa.ev@inapp.com>
2022-07-18 13:44:47 +0530

    Python files added

---
+++ Pythoninappdemo/test.py
@@ -0,0 +1,15 @@
+import sys
+
+#print("Hello World")
+#print(sys.version)
+
+#learning to comment
+#print("{} is my country. All Indians are my brothers and sisters".format('India'))
+#print("India is my country. All Indians are my brothers and sisters".count('India'))
+#mystring="Superman"
+#print(mystring.endswith('man'))
+#print(mystring.endswith('man',3))
+#print(mystring.endswith(('man','ma'),2,6))
+
+mystring2="Good morning"
+print(mystring2.find('Go'))
