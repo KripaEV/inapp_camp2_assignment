e6476bee5dbc8ddf4d585ac4beb736822f0f5a73
KripaVS <kripa.ev@inapp.com>
2022-07-19 12:58:27 +0530

    Assignment 4

---
+++ Pythoninappdemo/assignment4.py
@@ -0,0 +1,30 @@
+monthno=input("Enter your birth month number:")
+match(monthno):
+    case '1':
+        print("January-People born in January are bold and alert\nStone-Garnet")
+    case '2':
+        print("February-People born in February are lucky and loyal\nStone-Amethyst")
+    case '3':
+        print("March-People born in March are naughty and genuis\nStone-Aquamarine")
+    case '4':
+        print("April-People born in April are caring and strong\nStone-Diamond")
+    case '5':
+        print("May-People born in May are loving and practical\nStone-Emarald")
+    case '6':
+        print("June-People born in June are romantic and curious\nStone-Alexandrite")
+    case '7':
+        print("July-People born in July are adventurous and honest\nStone-Ruby")
+    case '8':
+        print("August-People born in August are active and hardworking\nStone-Peridot")
+    case '9':
+        print("September-People born in September are sensitive and pretty\nStone-Sapphire")
+    case '10':
+        print("October-People born in October are stylish and friendly\nStone-Tourmaline")
+    case '11':
+        print("November-People born in November are nice and creative\nStone-Citrine")
+    case '12':
+        print("December-People born in December are confident and freedom loving\nStone-Zircon")
+    case _:
+        print("Invalid month")
+    
+
